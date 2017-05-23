59df7749f1e5a582e159662ba3378bad49e2d005
Gagan Deep Singh <me@gagan.pro>
2017-05-23 15:40:33 +0530

    Corrected some more translation ready strings

--- includes/class-shortcode-widget.php
+++ includes/class-shortcode-widget.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Shortcode Widget Class
  *
@@ -7,9 +8,9 @@
 class Shortcode_Widget extends WP_Widget {
 
 	public function __construct() {
-		$widget_ops = array('classname' => 'shortcode_widget', 'description' => __('Shortcode or HTML or Plain Text.', SHORTCODE_WIDGET_TEXT_DOMAIN));
-		$control_ops = array('width' => 400, 'height' => 350);
-		parent::__construct('shortcode-widget', __('Shortcode Widget', SHORTCODE_WIDGET_TEXT_DOMAIN), $widget_ops, $control_ops);
+		$widget_ops	 = array( 'classname' => 'shortcode_widget', 'description' => __( 'Shortcode or HTML or Plain Text.', 'shortcode-widget' ) );
+		$control_ops = array( 'width' => 400, 'height' => 350 );
+		parent::__construct( 'shortcode-widget', __( 'Shortcode Widget', 'shortcode-widget' ), $widget_ops, $control_ops );
 	}
 
 	/**
@@ -18,22 +19,23 @@ class Shortcode_Widget extends WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 		/** This filter is documented in wp-includes/default-widgets.php */
-		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
-		
+		$title = apply_filters( 'widget_title', empty( $instance[ 'title' ] ) ? '' : $instance[ 'title' ], $instance, $this->id_base );
+
 		/**
 		 * Filter the content of the Text widget.
 		 *
 		 * @param string    $widget_text The widget content.
 		 * @param WP_Widget $instance    WP_Widget instance.
 		 */
-		$text = do_shortcode(apply_filters( 'widget_text', empty( $instance['text'] ) ? '' : $instance['text'], $instance ));
-		echo $args['before_widget'];
-		if ( ! empty( $title ) ) {
-			echo $args['before_title'] . $title . $args['after_title'];
-		} ?>
-			<div class="textwidget"><?php echo !empty( $instance['filter'] ) ? wpautop( $text ) : $text; ?></div>
+		$text = do_shortcode( apply_filters( 'widget_text', empty( $instance[ 'text' ] ) ? '' : $instance[ 'text' ], $instance ) );
+		echo $args[ 'before_widget' ];
+		if ( !empty( $title ) ) {
+			echo $args[ 'before_title' ] . $title . $args[ 'after_title' ];
+		}
+		?>
+		<div class="textwidget"><?php echo!empty( $instance[ 'filter' ] ) ? wpautop( $text ) : $text; ?></div>
 		<?php
-		echo $args['after_widget'];
+		echo $args[ 'after_widget' ];
 	}
 
 	/**
@@ -42,13 +44,13 @@ class Shortcode_Widget extends WP_Widget {
 	 * @return array
 	 */
 	public function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-		if ( current_user_can('unfiltered_html') )
-			$instance['text'] =  $new_instance['text'];
+		$instance			 = $old_instance;
+		$instance[ 'title' ]	 = strip_tags( $new_instance[ 'title' ] );
+		if ( current_user_can( 'unfiltered_html' ) )
+			$instance[ 'text' ]	 = $new_instance[ 'text' ];
 		else
-			$instance['text'] = stripslashes( wp_filter_post_kses( addslashes($new_instance['text']) ) ); // wp_filter_post_kses() expects slashed
-		$instance['filter'] = ! empty( $new_instance['filter'] );
+			$instance[ 'text' ]	 = stripslashes( wp_filter_post_kses( addslashes( $new_instance[ 'text' ] ) ) ); // wp_filter_post_kses() expects slashed
+		$instance[ 'filter' ]	 = !empty( $new_instance[ 'filter' ] );
 		return $instance;
 	}
 
@@ -56,17 +58,18 @@ class Shortcode_Widget extends WP_Widget {
 	 * @param array $instance
 	 */
 	public function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'text' => '' ) );
-		$title = strip_tags($instance['title']);
-		$text = esc_textarea($instance['text']);
-?>
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', SHORTCODE_WIDGET_TEXT_DOMAIN); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
+		$instance	 = wp_parse_args( (array) $instance, array( 'title' => '', 'text' => '' ) );
+		$title		 = strip_tags( $instance[ 'title' ] );
+		$text		 = esc_textarea( $instance[ 'text' ] );
+		?>
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'shortcode-widget' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" /></p>
 
 		<p><label for="<?php echo $this->get_field_id( 'text' ); ?>"><?php _e( 'Content:' ); ?></label>
-		<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>"><?php echo $text; ?></textarea></p>
+			<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>"><?php echo $text; ?></textarea></p>
 
-		<p><input id="<?php echo $this->get_field_id('filter'); ?>" name="<?php echo $this->get_field_name('filter'); ?>" type="checkbox" <?php checked(isset($instance['filter']) ? $instance['filter'] : 0); ?> />&nbsp;<label for="<?php echo $this->get_field_id('filter'); ?>"><?php _e('Automatically add paragraphs', SHORTCODE_WIDGET_TEXT_DOMAIN); ?></label></p>
-<?php
+		<p><input id="<?php echo $this->get_field_id( 'filter' ); ?>" name="<?php echo $this->get_field_name( 'filter' ); ?>" type="checkbox" <?php checked( isset( $instance[ 'filter' ] ) ? $instance[ 'filter' ] : 0 ); ?> />&nbsp;<label for="<?php echo $this->get_field_id( 'filter' ); ?>"><?php _e( 'Automatically add paragraphs', 'shortcode-widget' ); ?></label></p>
+		<?php
 	}
+
 }
