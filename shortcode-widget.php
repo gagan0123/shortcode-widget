07f346798603103992f50bf51e24710f3cec206f
Gagan Deep Singh <me@gagan.pro>
2017-05-23 15:39:32 +0530

    Changed directory structure, corrected translation ready strings

--- shortcode-widget.php
+++ shortcode-widget.php
@@ -1,30 +1,32 @@
 <?php
+
 /*
-Plugin Name: Shortcode Widget
-Plugin URI: http://wordpress.org/extend/plugins/shortcode-widget/
-Description: Adds a text-like widget that allows you to write shortcode in it. (Just whats missing in the default text widget)
-Author: Gagan Deep Singh
-Author URI: http://gagan.pro/
-Version: 1.2
-Text Domain: shortcode-widget
-License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-*/
+  Plugin Name: Shortcode Widget
+  Plugin URI: http://wordpress.org/extend/plugins/shortcode-widget/
+  Description: Adds a text-like widget that allows you to write shortcode in it. (Just whats missing in the default text widget)
+  Author: Gagan Deep Singh
+  Author URI: http://gagan.pro/
+  Version: 1.2
+  Text Domain: shortcode-widget
+  License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
 
-define('SHORTCODE_WIDGET_TEXT_DOMAIN','shortcode-widget');
+require_once('includes/class-shortcode-widget.php');
 
-require_once('class-shortcode-widget.php');
-
-function shortcode_widget_init(){
-	register_widget('Shortcode_Widget');
+function shortcode_widget_init() {
+	register_widget( 'Shortcode_Widget' );
 }
-add_action('widgets_init','shortcode_widget_init');
 
-function shortcode_widget_load_text_domain(){
-	load_plugin_textdomain( SHORTCODE_WIDGET_TEXT_DOMAIN, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+add_action( 'widgets_init', 'shortcode_widget_init' );
+
+function shortcode_widget_load_text_domain() {
+	load_plugin_textdomain( 'shortcode-widget', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
-add_action('plugins_loaded','shortcode_widget_load_text_domain');
 
-add_shortcode('shortcode_widget_test', 'shortcode_widget_test_output');
-function shortcode_widget_test_output($args){
-	return __( "It works" , SHORTCODE_WIDGET_TEXT_DOMAIN );
+add_action( 'plugins_loaded', 'shortcode_widget_load_text_domain' );
+
+add_shortcode( 'shortcode_widget_test', 'shortcode_widget_test_output' );
+
+function shortcode_widget_test_output( $args ) {
+	return __( "It works", 'shortcode-widget' );
 }
