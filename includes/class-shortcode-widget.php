88801559de7b005cd51e162efe9e1365210a20f0
Gagan Deep Singh <me@gagan.pro>
2017-08-28 06:59:55 +0530

    Escaping some output values that could had been overridden by the translations.

--- includes/class-shortcode-widget.php
+++ includes/class-shortcode-widget.php
@@ -30,7 +30,7 @@ class Shortcode_Widget extends WP_Widget {
 		$text = do_shortcode( apply_filters( 'widget_text', empty( $instance[ 'text' ] ) ? '' : $instance[ 'text' ], $instance ) );
 		echo $args[ 'before_widget' ];
 		if ( !empty( $title ) ) {
-			echo $args[ 'before_title' ] . $title . $args[ 'after_title' ];
+			echo $args[ 'before_title' ] . esc_html( $title ) . $args[ 'after_title' ];
 		}
 		?>
 		<div class="textwidget"><?php echo!empty( $instance[ 'filter' ] ) ? wpautop( $text ) : $text; ?></div>
@@ -44,12 +44,12 @@ class Shortcode_Widget extends WP_Widget {
 	 * @return array
 	 */
 	public function update( $new_instance, $old_instance ) {
-		$instance			 = $old_instance;
+		$instance				 = $old_instance;
 		$instance[ 'title' ]	 = strip_tags( $new_instance[ 'title' ] );
 		if ( current_user_can( 'unfiltered_html' ) )
-			$instance[ 'text' ]	 = $new_instance[ 'text' ];
+			$instance[ 'text' ]		 = $new_instance[ 'text' ];
 		else
-			$instance[ 'text' ]	 = stripslashes( wp_filter_post_kses( addslashes( $new_instance[ 'text' ] ) ) ); // wp_filter_post_kses() expects slashed
+			$instance[ 'text' ]		 = stripslashes( wp_filter_post_kses( addslashes( $new_instance[ 'text' ] ) ) ); // wp_filter_post_kses() expects slashed
 		$instance[ 'filter' ]	 = !empty( $new_instance[ 'filter' ] );
 		return $instance;
 	}
@@ -58,17 +58,15 @@ class Shortcode_Widget extends WP_Widget {
 	 * @param array $instance
 	 */
 	public function form( $instance ) {
-		$instance	 = wp_parse_args( (array) $instance, array( 'title' => '', 'text' => '' ) );
-		$title		 = strip_tags( $instance[ 'title' ] );
-		$text		 = esc_textarea( $instance[ 'text' ] );
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'text' => '' ) );
 		?>
-		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'shortcode-widget' ); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" /></p>
+		<p><label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Title:', 'shortcode-widget' ); ?></label>
+			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $instance[ 'title' ] ); ?>" /></p>
 
-		<p><label for="<?php echo $this->get_field_id( 'text' ); ?>"><?php _e( 'Content:' ); ?></label>
-			<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>"><?php echo $text; ?></textarea></p>
+		<p><label for="<?php echo esc_attr( $this->get_field_id( 'text' ) ); ?>"><?php esc_html_e( 'Content:', 'shortcode-widget' ); ?></label>
+			<textarea class="widefat" rows="16" cols="20" id="<?php echo esc_attr( $this->get_field_id( 'text' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'text' ) ); ?>"><?php echo esc_textarea( $instance[ 'text' ] ) ?></textarea></p>
 
-		<p><input id="<?php echo $this->get_field_id( 'filter' ); ?>" name="<?php echo $this->get_field_name( 'filter' ); ?>" type="checkbox" <?php checked( isset( $instance[ 'filter' ] ) ? $instance[ 'filter' ] : 0 ); ?> />&nbsp;<label for="<?php echo $this->get_field_id( 'filter' ); ?>"><?php _e( 'Automatically add paragraphs', 'shortcode-widget' ); ?></label></p>
+		<p><input id="<?php echo esc_attr( $this->get_field_id( 'filter' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'filter' ) ); ?>" type="checkbox" <?php checked( isset( $instance[ 'filter' ] ) ? $instance[ 'filter' ] : 0  ); ?> />&nbsp;<label for="<?php echo esc_attr( $this->get_field_id( 'filter' ) ); ?>"><?php esc_html_e( 'Automatically add paragraphs', 'shortcode-widget' ); ?></label></p>
 		<?php
 	}
 
