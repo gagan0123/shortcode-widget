f034f69af0e2cb153cc994db107d49b8058e922e
Gagan Deep Singh <me@gagan.pro>
2017-08-28 07:00:25 +0530

    Adding Grunt support

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function ( grunt ) {
+
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+		wp_readme_to_markdown: {
+			dist: {
+				options: {
+					screenshot_url: '<%= pkg.repository.url %>/raw/master/assets/{screenshot}.png',
+					post_convert: function ( file ) {
+						return "<img src='" + grunt.config.get( 'pkg' ).repository.url + "/raw/master/assets/icon-128x128.png' align='right' />\n\n" + file;
+					}
+				},
+				files: {
+					'README.md': 'readme.txt'
+				}
+			}
+		},
+		makepot: {
+			target: {
+				options: {
+					domainPath: 'languages',
+					exclude: [ 'node_modules/.*', 'tests/.*' ],
+					mainFile: '<%= pkg.main %>',
+					potFilename: '<%= pkg.name %>.pot',
+					potHeaders: {
+						poedit: false,
+						'report-msgid-bugs-to': '<%= pkg.bugs.url %>'
+					},
+					type: 'wp-plugin',
+					updateTimestamp: false
+				}
+			}
+		}
+	} );
+
+	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+
+	grunt.registerTask( 'default', [
+		'wp_readme_to_markdown', 'makepot'
+	] );
+
+};
